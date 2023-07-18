cb1d9cc4035461aeb10bbd3008317a964cb36245
AJAYI Toheeb Opeyemi <toheebopeyemi9@gmail.com>
2023-07-18 16:24:56 +0100

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+    const [active, setActive] = useState('all')
 
     return (
         <nav className='container' id='navbar'>
             <div className='hstack justify-between text-center'>
-                <Link className='nav-link active' to="/">All</Link>
-                <Link className='nav-link' to="/active">Active</Link>
-                <Link className='nav-link' to="/completed">Completed</Link>
+                <Link className={`nav-link ${active === 'all'? 'active': ''}`} to="/" onClick={() => setActive('all')}>All</Link>
+                <Link className={`nav-link ${active === 'active' ? 'active' : ''}`} to="/active" onClick={() => setActive('active')}>Active</Link>
+                <Link className={`nav-link ${active === 'completed' ? 'active' : ''}`} to="/completed" onClick={() => setActive('completed')}>Completed</Link>
             </div>
         </nav>
     )
